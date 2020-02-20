5b0d6240d06c6b1bffadb3029c28838297aca0b5
Mindy Zwanziger <mindy.zwanziger@sitkatech.com>
2020-02-20 15:15:49 -0800

    Add and set up database

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -8,10 +8,22 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers().ToList();
+            var customers = _context.Customers.ToList();
 
             var viewModel = new CustomerViewModel() { Customers = customers };
 
@@ -21,21 +33,12 @@ namespace Vidly.Controllers
         // GET: Customers/Detail/Id
         public ActionResult Detail(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 }
