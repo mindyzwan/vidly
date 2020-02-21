f17d9202ad2c7ddbbd321631b69805e2c821674b
Mindy Zwanziger <mindyzwan@gmail.com>
2020-02-21 10:47:49 -0800

    Add Genre Table

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,5 +9,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
     }
 }
