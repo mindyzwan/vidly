181e253051b31cb8f8828d8ed7b094825bcc9dfd
Mindy Zwanziger <mindyzwan@gmail.com>
2020-02-21 10:09:37 -0800

    Add birth date to customers table

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,15 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
-        [StringLength(255)]
-        public string Name { get; set; }
+        [Required] [StringLength(255)] public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+        public DateTime BirthDate { get; set; }
     }
 }   
