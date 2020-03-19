57ee7d5d03ca2b635d74ee7965380383af8fd995
mindyzwan <mindyzwan@gmail.com>
2020-03-18 18:09:11 -0700

    Add new movie action

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -20,6 +20,36 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
+
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.QuantityInStock = movie.QuantityInStock;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+            return View("CustomerForm", viewModel);
+        }
+
         // GET: Movies
         public ViewResult Index()
         {
